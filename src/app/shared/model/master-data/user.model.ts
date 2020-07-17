8d33b85f0659595d4150b7782a8962c0046af237
Mahmoud Elsaka <Mahmoud.elskaa20@gmail.com>
2020-07-17 23:35:51 +0200

    change project name in pom.xml

--- src/app/shared/model/master-data/user.model.ts
+++ src/app/shared/model/master-data/user.model.ts
@@ -1,11 +1,21 @@
 import { AuthorityModel } from './authority.model';
 
 export class UserModel {
+  id: number;
   userName: string;
+  firstName: string;
+  lastName: string;
+  jobDescription: string;
+  userLevelId: number;
+  active: number;
+  activeFlag: boolean;
+  lineManagerId: number;
+  branchNo: string;
+  isS2AccountFlag: boolean;
+  isS2Account: number;
   password: string;
+  confirmedPassword: string;
   authorities: AuthorityModel[];
-  encrypted: boolean;
-  token: string;
 
   // constructor(
   //   username?: string,
